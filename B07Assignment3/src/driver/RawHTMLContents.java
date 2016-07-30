413889f942d0e53e104b70ec9b528a47ab9fc820
kushtovm@UTORONTO.CA <kushtovm@UTORONTO.CA@c3d25fe1-09b3-451c-bde6-691dab5edbce>
2016-07-30 03:49:27 +0000

    saving all files

---
+++ B07Assignment3/src/driver/RawHTMLContents.java
@@ -0,0 +1,28 @@
+package driver;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class RawHTMLContents {
+  
+  public static String getHTML(String urlString) throws Exception {
+    // create object to store html source text as it is being collected
+    StringBuilder html = new StringBuilder();
+    // open connection to given url
+    URL url = new File(urlString).toURI().toURL(); 
+    // create BufferedReader to buffer the given url's HTML source
+    BufferedReader htmlbr =
+        new BufferedReader(new InputStreamReader(url.openStream()));
+    String line;
+    // read each line of HTML code and store in StringBuilder
+    while ((line = htmlbr.readLine()) != null) {
+      html.append(line);
+    }
+    htmlbr.close();
+    // convert StringBuilder into a String and return it
+    return html.toString();
+   }
+  
+}
