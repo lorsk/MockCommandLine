c2aad8c3e842f28d559e6d503e173fd3231f04f4
kushtovm@UTORONTO.CA <kushtovm@UTORONTO.CA@c3d25fe1-09b3-451c-bde6-691dab5edbce>
2016-07-31 20:11:08 +0000

    added code for test cases

---
+++ B07Assignment3/src/test/ExtractNumberOfCitationsTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExtractNumberOfCitationsTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
